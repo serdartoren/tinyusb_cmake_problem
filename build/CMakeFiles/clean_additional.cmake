92e8709909f42abdbae1e9cc210d6c20fbde094e
serdartoren <42543700+serdartoren@users.noreply.github.com>
2024-05-27 13:01:35 +0300

    start

---
+++ build/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,22 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "")
+  file(REMOVE_RECURSE
+  "bootloader/bootloader.bin"
+  "bootloader/bootloader.elf"
+  "bootloader/bootloader.map"
+  "config/sdkconfig.cmake"
+  "config/sdkconfig.h"
+  "esp-idf/esptool_py/flasher_args.json.in"
+  "esp-idf/mbedtls/x509_crt_bundle"
+  "flash_app_args"
+  "flash_bootloader_args"
+  "flash_project_args"
+  "flasher_args.json"
+  "idf_as_lib.bin"
+  "ldgen_libraries"
+  "ldgen_libraries.in"
+  "x509_crt_bundle.S"
+  )
+endif()
---
+++ build/esp-idf/mbedtls/mbedtls/library/error.c
@@ -0,0 +1,1 @@
+/home/ben/esp/esp-idf/components/mbedtls/mbedtls/library/error.c
